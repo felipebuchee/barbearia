a5730b2fd17b9cff2f7da0e4a6874bc2cc90c1ff
felipebuchee <felipe10buche@gmail.com>
2024-12-17 09:39:14 -0300

    Pagina inicial

--- splash.js
+++ splash.js
@@ -3,5 +3,5 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => {
         splash.classList.add('display-none');
-    }, 1);
+    }, 5000);
 });
