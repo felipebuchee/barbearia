3c49efdc8707710339cf5ef70cdb40142c2ccc95
felipebuchee <felipe10buche@gmail.com>
2024-12-16 23:14:48 -0300

    .

--- expand.js
+++ expand.js
@@ -1,8 +1,43 @@
-//expande o menu
+class MobileNavBar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList)
+        this.navLinks = document.querySelectorAll(navLinks)
+        this.activeClass = "active"
 
-var expand = document.querySelector('#expand')
-var menuLateral = document.querySelector ('.menu-lat')
+        this.handleClick = this.handleClick.bind(this)
+    }
 
-expand.addEventListener('click', function(){
-    menuLateral.classList.toggle('expandir')
-})
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass)
+        this.mobileMenu.classList.toggle(this.activeClass)
+        this.animateLinks()
+    }
+
+    animateLinks(){
+        this.navLinks.forEach(link => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = 'navLinkFade 0.5s ease forwards 0.3s')
+    })
+} 
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick)
+    }
+
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent()
+        }
+        return this
+    }
+}
+
+const mobileNavBar = new MobileNavBar(
+    '.mobile-menu',
+    '.nav-list',
+    '.nav-list li'
+)
+
+mobileNavBar.init()
